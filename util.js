83977a6a3968e51ad792026b04049baace8bbd10
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-03-10 11:40:11 +0100

    added function addMonths

--- util.js
+++ util.js
@@ -60,4 +60,27 @@ export default class Util {
         }
     }
 
+    //https://stackoverflow.com/a/12793246+
+    //TODO: correct using comments
+    addMonths(date, months) {
+        let d = date.getDate();
+        date.setMonth(date.getMonth() + +months);
+        if (date.getDate() != d) {
+          date.setDate(0);
+        }
+        return date;
+      
+      // Add 12 months to 29 Feb 2016 -> 28 Feb 2017
+      console.log(addMonths(new Date(2016,1,29),12).toString());
+      
+      // Subtract 1 month from 1 Jan 2017 -> 1 Dec 2016
+      console.log(addMonths(new Date(2017,0,1),-1).toString());
+      
+      // Subtract 2 months from 31 Jan 2017 -> 30 Nov 2016
+      console.log(addMonths(new Date(2017,0,31),-2).toString());
+      
+      // Add 2 months to 31 Dec 2016 -> 28 Feb 2017
+      console.log(addMonths(new Date(2016,11,31),2).toString());
+    }
+
 }
