5fc97ed60a60cf917b09fdba8a06102b36eaeecb
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-04-26 17:14:15 +0200

    removed static events

--- bootstrap-calendar-master/bootstrap-calendar-master/js/calendar.js
+++ bootstrap-calendar-master/bootstrap-calendar-master/js/calendar.js
@@ -8,54 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			center: 'title',
 			right: 'dayGridMonth,timeGridWeek,timeGridDay'
 		},
-		events: [
-			{
-				title: 'All Day Event',
-				start: '2024-03-01'
-			},
-			{
-				title: 'Long Event',
-				start: '2024-03-07',
-				end: '2024-03-10'
-			},
-			{
-				groupId: '999',
-				title: 'Repeating Event',
-				start: '2024-03-09T16:00:00'
-			},
-			{
-				groupId: '999',
-				title: 'Repeating Event',
-				start: '2024-03-16T16:00:00'
-			},
-			{
-				title: 'Conference',
-				start: '2024-03-11',
-				end: '2024-03-13'
-			},
-			{
-				title: 'Meeting',
-				start: '2024-03-12T10:30:00',
-				end: '2024-03-12T12:30:00'
-			},
-			{
-				title: 'Lunch',
-				start: '2024-03-12T12:00:00'
-			},
-			{
-				title: 'Meeting',
-				start: '2024-03-12T14:30:00'
-			},
-			{
-				title: 'Birthday Party',
-				start: '2024-03-13T07:00:00'
-			},
-			{
-				title: 'Click for Google',
-				url: 'https://google.com/',
-				start: '2024-03-28'
-			}
-		]
+		events: 'events.json'
 	});
 	calendar.render();
 });
