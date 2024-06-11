cc7608772ca46d5e83bcf5ec8a150e16b95b5df0
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-06-11 20:35:09 +0200

    removed unneeded declaration

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css',
-            'resources/css/calendar.css',
-            'resources/sass/calendar.scss',
-            'resources/js/app.js'],
-            refresh: true,
-        }),
+
     ],
 });
