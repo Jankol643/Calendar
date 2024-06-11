15f8f562cbfca6bc882ad49ae0ef5cf2a166220c
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-06-11 20:35:31 +0200

    initial commit

---
+++ database/migrations/2024_06_11_152350_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('recurr');
+            $table->integer('freq_no');
+            $table->integer('freq_dur');
+            $table->dateTime('last_exec');
+            $table->dateTime('due_date');
+            $table->string('task_cat');
+            $table->string('task_name');
+            $table->string('task_descr');
+            $table->integer('task_dur');
+            $table->integer('prio');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
