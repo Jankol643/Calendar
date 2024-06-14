44b5086f36c5752f4a07fa7382e0d89a84a38415
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-06-14 19:57:18 +0200

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-
+        laravel({
+            input: ['resources/css/app.css',
+                'resources/css/calendar.css',
+                'resources/sass/calendar.scss',
+                'resources/js/app.js'],
+            refresh: true,
+        }),
     ],
 });
