4a2bdad5a9a30e88afc875a59e9e2ce369fea005
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-06-16 18:27:26 +0200

    initial commit

---
+++ resources/views/edit-task.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="modal fade" id="editTaskModal" tabindex="-1" aria-labelledby="editTaskModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editTaskModalLabel">Edit Task</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('tasks.update', $task->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+
+                        <!-- Input fields for each attribute of the Task model -->
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" value="{{ $task->name }}">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="start_date" class="form-label">Start Date</label>
+                            <input type="date" class="form-control" id="start_date" name="start_date" value="{{ $task->start_date }}">
+                        </div>
+
+                        <!-- Add input fields for other attributes -->
+
+                        <button type="submit" class="btn btn-primary">Save Changes</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
