8bbdb8451ae74df38a19dd3b44bd9c36d6ce1d9a
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-07-05 15:27:45 +0200

    added list view for entries and events

--- resources/views/calendar/entry_list.blade.php
+++ resources/views/calendar/entry_list.blade.php
@@ -1,3 +1,55 @@
-<?php
-foreach ($entries as $entry) {
-}
+<!-- Table for Calendar Entries -->
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Category</th>
+            <th scope="col">Start Date</th>
+            <th scope="col">End Date</th>
+            <th scope="col">User ID</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($entries as $entry)
+        <tr>
+            <th scope="row">{{ $entry->id }}</th>
+            <td>{{ $entry->title }}</td>
+            <td>{{ $entry->description }}</td>
+            <td>{{ $entry->category }}</td>
+            <td>{{ $entry->start_date }}</td>
+            <td>{{ $entry->end_date }}</td>
+            <td>{{ $entry->user_id }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+<!-- Table for Events -->
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Start Date</th>
+            <th scope="col">End Date</th>
+            <th scope="col">All Day</th>
+            <th scope="col">User ID</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($events as $event)
+        <tr>
+            <th scope="row">{{ $event->id }}</th>
+            <td>{{ $event->title }}</td>
+            <td>{{ $event->description }}</td>
+            <td>{{ $event->start_date }}</td>
+            <td>{{ $event->end_date }}</td>
+            <td>{{ $event->all_day }}</td>
+            <td>{{ $event->user_id }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
