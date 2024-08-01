2022f487e8dba0a7cc10b66ea85c62e27ec7e9b4
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-08-01 18:03:18 +0200

    added api route to get events

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ use App\Http\Controllers\EventController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/api/events', [EventController::class, 'getEvents']);
