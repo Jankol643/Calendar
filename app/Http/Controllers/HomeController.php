6eafb3544593b8c366d80e93c0fc4cbd3c9bd945
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-09-05 15:20:46 +0200

    simplified index function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\Task;
 
 class HomeController extends Controller {
     /**
@@ -15,23 +16,14 @@ class HomeController extends Controller {
     }
 
     public function index() {
+        // Retrieve all events and tasks
         $events = Event::all();
+        $tasks = Task::all();
 
-        // Convert the events to an array of objects with all parameters
-        $events = $events->map(function ($event) {
-            return (object) [
-                'id' => $event->id,
-                'title' => $event->title,
-                'description' => $event->description,
-                'start_date' => $event->start_date instanceof \DateTime ? $event->start_date->format('Y-m-d H:i:s') : null,
-                'end_date' => $event->end_date instanceof \DateTime ? $event->end_date->format('Y-m-d H:i:s') : null,
-                'all_day' => $event->all_day,
-                'user_id' => $event->user_id
-            ];
-        });
-
-        return view('calendar.index', compact('events'));
+        // Pass the collections to the view
+        return view('calendar.index', compact('events', 'tasks'));
     }
+
 
     /**
      * Show the welcome page.
