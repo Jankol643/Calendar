4e47bbbae32b0dd0d550cd9270f172c6e981463e
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-09-05 15:35:38 +0200

    edited route

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api/events', [EventController::class, 'getEvents']);
+Route::get('/events', [EventController::class, 'getFullCalendarEvents']);
