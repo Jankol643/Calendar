16c1aadf9355d1b725b70ad2ed12934b4f8e9836
Jankol643 <61902737+Jankol643@users.noreply.github.com>
2024-09-11 09:04:20 +0200

    removed unique constraint

--- database/migrations/2024_08_08_042754_create_categories_table.php
+++ database/migrations/2024_08_08_042754_create_categories_table.php
@@ -8,7 +8,7 @@ class CreateCategoriesTable extends Migration {
     public function up() {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
